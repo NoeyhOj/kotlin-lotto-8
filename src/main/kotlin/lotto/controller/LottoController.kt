161ac98539d4d0e744937069d96fef3dd3ffabf5
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-11-02 13:53:44 +0900

    style: 코드의 주석 및 줄 삭제 등 코드 정리

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -13,24 +13,15 @@ class LottoController(
 ) {
     fun run() {
         try {
-            // 로또를 금액만큼 구매
             val lottoPrice = inputController.priceParser(view.showPriceInput())
             view.showBuyLottoCount(lottoPrice / 1000)
             val lottoList = buyLotto(lottoPrice / 1000)
             view.showBuyLotto(lottoList)
-            // 당첨 번호와 보너스 번호 입력
-            val winningLotto =
-                Lotto(inputController.winningNumberParser(view.showWinningNumberInput()))
-            val bonusNumber =
-                inputController.bonusParser(view.showBonusInput(), winningLotto.getNumbers())
-            // 등수를 확인하는 알고리즘
+            val winningLotto = Lotto(inputController.winningNumberParser(view.showWinningNumberInput()))
+            val bonusNumber = inputController.bonusParser(view.showBonusInput(), winningLotto.getNumbers())
             val winningLottoList = lottoList.map { lotto ->
-                LottoRank.getRank(
-                    winningLotto.getCountNumbers(lotto),
-                    lotto.getContainBonus(bonusNumber)
-                )
+                LottoRank.getRank(winningLotto.getCountNumbers(lotto), lotto.getContainBonus(bonusNumber))
             }
-            // 수익률
             val roi = round(winningLottoList.sumOf { it.price }.toDouble() / lottoPrice * 1000) / 10
             view.showResult(winningLottoList, roi)
         } catch (e: IllegalArgumentException) {
