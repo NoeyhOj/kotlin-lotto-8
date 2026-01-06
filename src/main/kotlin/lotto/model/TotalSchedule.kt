f77fa92710763554c6a986b7225845a9629656e7
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-01-06 23:00:21 +0900

    retactor: getEmployeesList()함수의 역할을 줄이고, checkInfo()함수를 추가함

--- src/main/kotlin/lotto/model/TotalSchedule.kt
+++ src/main/kotlin/lotto/model/TotalSchedule.kt
@@ -7,52 +7,41 @@ class TotalSchedule(private val weekdays: List<String>, private val weekends: Li
         require(weekdays.toSet() == weekends.toSet()) { "[ERROR] 평일과 주말의 근무자 이름 수가 다릅니다." }
     }
 
+    val totalSchedule = mutableListOf<Info>()
+
+
+
     fun getEmployeesList(): MutableList<Info> {
+        var beforeName = ""
+        var weekdayList = weekdays.toMutableList()
+        var weekendList = weekends.toMutableList()
         var weekdayCount = 0
         var weekendCount = 0
-        val weekdays = weekdays.toMutableList()
-        val weekends = weekends.toMutableList()
-        var beforeName = ""
-        val totalSchedule = mutableListOf<Info>()
         schedule.forEach { total ->
-            if (total.holi) { // 휴일
-                if (beforeName == weekends[weekendCount % weekends.size]) {
-                    var cur = weekends[weekendCount % weekends.size]
-                    var aft = weekends[weekendCount+1 % weekends.size]
-                    cur = aft.also { aft = cur }
-                    weekends[weekendCount % weekends.size] = cur
-                    weekends[weekendCount+1 % weekends.size] = aft
-                }
-                totalSchedule.add(Info(weekends[weekendCount % weekends.size], total.day, total.days.dayKor+"(휴일)"))
-                beforeName = weekends[weekendCount % weekends.size]
-                weekendCount++
+            if (total.holi || !total.isDays) {
+                val token = checkInfo(total, beforeName, weekendList, weekendCount)
+                beforeName = token.first
+                weekendList = token.second
+                weekendCount = token.third
             } else {
-                if (Days.getWeekdays().contains(total.days)) {
-                    if (beforeName == weekdays[weekdayCount % weekdays.size])  {
-                        var cur = weekdays[weekdayCount % weekdays.size]
-                        var aft = weekdays[weekdayCount+1 % weekdays.size]
-                        cur = aft.also { aft = cur }
-                        weekdays[weekdayCount % weekdays.size] = cur
-                        weekdays[weekdayCount+1 % weekdays.size] = aft
-                    }
-                    totalSchedule.add(Info(weekdays[weekdayCount % weekdays.size], total.day, total.days.dayKor))
-                    beforeName = weekdays[weekdayCount % weekdays.size]
-                    weekdayCount++
-                } else {
-                    if (beforeName == weekends[weekendCount % weekends.size]) {
-                        var cur = weekends[weekendCount % weekends.size]
-                        var aft = weekends[weekendCount+1 % weekends.size]
-                        cur = aft.also { aft = cur }
-                        weekends[weekendCount % weekends.size] = cur
-                        weekends[weekendCount+1 % weekends.size] = aft
-                    }
-                    totalSchedule.add(Info(weekends[weekendCount % weekends.size], total.day, total.days.dayKor))
-                    beforeName = weekends[weekendCount % weekends.size]
-                    weekendCount++
-                }
+                val token = checkInfo(total, beforeName, weekdayList, weekdayCount)
+                beforeName = token.first
+                weekdayList = token.second
+                weekdayCount = token.third
             }
-
         }
         return totalSchedule
+    }
+
+    fun checkInfo(total: Total, beforeName: String, employeeList: MutableList<String>, count: Int): Triple<String, MutableList<String>, Int> {
+        if (beforeName == employeeList[count % employeeList.size]) {
+            var cur = employeeList[count % employeeList.size]
+            var aft = employeeList[count+1 % employeeList.size]
+            cur = aft.also { aft = cur }
+            employeeList[count % employeeList.size] = cur
+            employeeList[count + 1 % employeeList.size] = aft
+        }
+        totalSchedule.add(Info(employeeList[count % employeeList.size], total.day, if (total.holi) total.days.dayKor+"(휴일)" else total.days.dayKor))
+        return Triple(employeeList[count % employeeList.size], employeeList, count+1)
     }
 }
